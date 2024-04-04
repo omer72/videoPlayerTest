6920b8c1759b1040071e2fa0613086c660891327
Omer Etrog <omer72@gmail.com>
2024-04-04 19:17:00 +0300

    clean events

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,56 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import poster from './assets/poster.png';
 
+interface TimeRange{
+  start:number;
+  end:number
+}
+
 function App() {
 
   const [videoUrl, setVideoUrl] = useState('');
   const [videoDuration , setVideoDuration] = useState(0);
   const [videoCurrentTime , setVideoCurrentTime] = useState(0);
-  const [videoBufferedTime , setVideoBufferedTime] = useState(0);
+  const [videoBufferedTime, setVideoBufferedTime] = useState<TimeRange[]>([]);
   const videoRef = useRef<HTMLVideoElement>(null);
 
 
   const url = 'https://mirror.clarkson.edu/blender/demo/movies/BBB/bbb_sunflower_1080p_30fps_normal.mp4';
 
   useEffect(()=>{
-    const video:HTMLVideoElement | null = videoRef.current;
+    const video = videoRef.current;
     if (video){
-      video.addEventListener("loadedmetadata", function () {
-        setVideoDuration(video.duration);
-        
 
-    });
-    video.addEventListener("timeupdate", function () {
-        setVideoCurrentTime(video.currentTime)
-    });
-    video.addEventListener("progress", function(){
-      setVideoBufferedTime(video.buffered.end(0));
-    })
-    }
+      const handleLoadedMetadata = () => setVideoDuration(video.duration);
+      const handleTimeUpdate = () => setVideoCurrentTime(video.currentTime);
+      const handleProgress = () => {
+        const bufferedSize = video.buffered.length;
+        let videoBufferedTimeClone = videoBufferedTime;
+        if (videoBufferedTimeClone.length <  bufferedSize) {
+          videoBufferedTimeClone.push({
+            start: video.buffered.start(bufferedSize - 1),
+            end: video.buffered.end(bufferedSize - 1)
+          })
+        }else{
+          videoBufferedTimeClone[bufferedSize - 1].start = video.buffered.start(bufferedSize - 1);
+          videoBufferedTimeClone[bufferedSize - 1].end = video.buffered.end(bufferedSize - 1);
+        }
+        setVideoBufferedTime(videoBufferedTimeClone);
+      };
 
+      video.addEventListener("loadedmetadata", handleLoadedMetadata);
+      video.addEventListener("timeupdate", handleTimeUpdate);
+      video.addEventListener("progress", handleProgress);
+      
+    
+    return () => {
+      if (video){
+        video.removeEventListener("loadedmetadata", handleLoadedMetadata);
+        video.removeEventListener("timeupdate", handleTimeUpdate);
+        video.removeEventListener("progress", handleProgress);
+      }
+    };
+  }
     
   },[])
 
@@ -45,16 +68,19 @@ function App() {
   }
   return (
     <div className='videoPlayer'>
-      
       <video ref={videoRef} width="600" height="240" controls src={videoUrl} poster={poster}/>
       <button className='videoButton' onClick={handleClick}>Load Url</button>
       <div className='stateIndications'>
         <div className='duration'></div>
-        <div className='buffered' style={{width:((videoBufferedTime/videoDuration)*100+ '%')}}></div>
+        {videoBufferedTime.map((buffredTime)=> (
+           <div className='buffered' style={{
+            left:`${(buffredTime.start / videoDuration) * 100}%`,
+             width: `${((buffredTime.end - buffredTime.start) / videoDuration) * 100}%` }}></div>
+
+        ))}
         <div className='played' style={{width:((videoCurrentTime/videoDuration)*100+ '%')}}></div>
       </div>
-      {videoDuration} {videoCurrentTime} {videoBufferedTime}
-            </div>
+    </div>
   )
 }
 
